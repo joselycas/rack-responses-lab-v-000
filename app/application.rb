6d07f5790a7965e2c3f63666fada5b6215188191
Jos Castro <joselycas@gmail.com>
2018-09-13 13:58:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,8 @@
 class Application
   
   def call(env)
+     resp = Rack::Response.new
+     
     if(hour >= 6 && hour < 12){
             greeting = "Good Morning";
         } else if(hour>= 12 && hour < 17){
