1326334c60c80665d506e6010762487f0d0dcfa8
Jos Castro <joselycas@gmail.com>
2018-09-13 14:10:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,9 @@ class Application
    resp = Rack::Response.new
    
    time = Time.now
-    if time >= 6 && time < 12
+    if time >= 12
            resp.write "Good Morning"
-    elsif  time >= 12
+    elsif  time <= 12
            resp.write "Good Afternoon"
     end
          resp.finish
