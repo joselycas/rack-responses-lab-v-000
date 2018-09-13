e71d31c579275cc77c66d16c1db98e709b7e6316
Jos Castro <joselycas@gmail.com>
2018-09-13 14:14:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
    resp = Rack::Response.new
    
-   time = Time.now
+   t = Time.now
     if time >= 12
            resp.write "Good Morning"
     elsif  time <= 12
