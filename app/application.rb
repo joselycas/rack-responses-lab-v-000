effa63fb71bba894ad9a6be212290bec751e1b1d
Jos Castro <joselycas@gmail.com>
2018-09-13 13:58:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,10 @@
 class Application
   
   def call(env)
+    if(hour >= 6 && hour < 12){
+            greeting = "Good Morning";
+        } else if(hour>= 12 && hour < 17){
+            greeting = "Good Afternoon"
   end
   
 end
